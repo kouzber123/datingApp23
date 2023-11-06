77af0e742da66545ef41fefcf77b30355fa6a516
kouzber123 <tom.nguyen@windowslive.com>
2023-11-06 19:15:58 +0200

    feature: edit template form for put

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
@@ -37,6 +38,21 @@ namespace API.Controllers
 
         }
 
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+                //claimTypes come directly  from JWT
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _user.GetUserByUsernameAsync(username);
+
+            if (user is null) return NotFound();
+
+            _mapper.Map(memberUpdateDto, user);
+            if(await _user.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }
         // [HttpGet("{id}")]
         // public async Task<ActionResult<MemberDto>> GetUserById(int id)
         // {
--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxSpinnerModule } from 'ngx-spinner';
 @NgModule({
   declarations: [],
   imports: [
@@ -12,7 +13,10 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
     }),
+    NgxSpinnerModule.forRoot({
+      type: 'line-scale-pulse-out-rapid',
+    }),
   ],
-  exports: [BsDropdownModule, ToastrModule, TabsModule],
+  exports: [BsDropdownModule, ToastrModule, TabsModule, NgxSpinnerModule],
 })
 export class SharedModule {}
