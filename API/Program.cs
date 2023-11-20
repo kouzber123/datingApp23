8f0adcd80e174e34e671e9aa5e22e408ab76232e
kouzber123 <tom.nguyen@windowslive.com>
2023-11-20 10:18:17 +0200

    test

--- API/Program.cs
+++ API/Program.cs
@@ -19,7 +19,17 @@ else
 {
     // Use connection string provided at runtime by FlyIO.
     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+    /*
+ Username:    postgres
+  Password:    DcyAsirACWfGCcq
+  Hostname:    datingapp2.internal
+  Flycast:     fdaa:3:69aa:0:1::f
+  Proxy port:  5432
+  Postgres port:  5433
+  Connection string: postgres://postgres:DcyAsirACWfGCcq@datingapp2.flycast:5432
 
+
+    */
     // Parse connection URL to connection string for Npgsql
     connUrl = connUrl.Replace("postgres://", string.Empty);
     var pgUserPass = connUrl.Split("@")[0];
@@ -32,7 +42,7 @@ else
     var pgPort = pgHostPort.Split(":")[1];
     var updatedHost = pgHost.Replace("flycast", "internal");
 
-    connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+    connString = $"Server=datingapp2.internal;Port=5432;User Id=postgres;Password=DcyAsirACWfGCcq;Database=datingapp2;";
 }
 builder.Services.AddDbContext<DataContext>(opt =>
 {
